3819c34dab0eeefe6115b431f0bf12d33f0eb8c5
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-10-16 15:22:03 -0400

    Refactored AI a little bit

--- src/ai.js
+++ src/ai.js
@@ -1,53 +1,37 @@
-import {
-  changeNumber, ValidationStates
-}
-from './actions';
+import {changeNumber, ValidationStates} from './actions';
 
-let delay = 100;
+let delay = 500;
 
 function AI(store) {
-  let {
-    puzzle
-  } = store.getState();
-  //  console.log('BOOYAH', store, puzzle);
-
-  let unTried = [];
-  for (let i = 0; i < puzzle.total / 2; i++) {
-    unTried.push(i + 1);
-  }
-  let tried = [];
-
-  let trying = unTried[0];
-  unTried.shift();
-  setTimeout(() => pushToGrid(trying), delay);
+  let {puzzle} = store.getState();
+  let unTried, tried;
 
   store.subscribe(() => {
     puzzle = store.getState().puzzle;
     console.log('HEYO', unTried, tried);
-
-    if (puzzle.complete === ValidationStates.EMPTY) {
-      if (unTried.length > 0) {
-        unTried = [...tried, ...unTried];
-        tried = [];
-        let trying = unTried[0];
-        unTried.shift();
-        setTimeout(() => pushToGrid(trying), delay);
-      } else {
-        setTimeout(() => tried.push(popFromGrid()), delay);
-      }
-    } else if (puzzle.complete === ValidationStates.INVALID) {
-      setTimeout(() => tried.push(popFromGrid()), delay);
-    }
+    decide();
   });
-
+  
+  init();
+  decide();
+  
+  function init(){
+    unTried = [];
+    tried = [];
+    
+    for (let i = puzzle.total / 2; i > 0 ; i--) {
+      unTried.push(i);
+    }
+  }
 
   function pushToGrid(newNode) {
     let done = false;
+    
     for (let i = 0; !done && i < puzzle.numbers.length; i++) {
       for (let j = 0; j < puzzle.numbers[i].length; j++) {
         if (puzzle.numbers[i][j] === 0) {
           console.log('PUSH', newNode);
-          store.dispatch(changeNumber(i, j, newNode));
+          setTimeout(() => store.dispatch(changeNumber(i, j, newNode)), delay);
           done = true;
           break;
         }
@@ -58,6 +42,7 @@ function AI(store) {
   function popFromGrid() {
     let done = false;
     let result, resultI, resultJ;
+    
     for (let i = 0; !done && i < puzzle.numbers.length; i++) {
       for (let j = 0; j < puzzle.numbers[i].length; j++) {
         if (puzzle.numbers[i][j] === 0) {
@@ -72,10 +57,24 @@ function AI(store) {
     }
 
     console.log('POP', result);
-    store.dispatch(changeNumber(resultI, resultJ, 0));
+    setTimeout(() => store.dispatch(changeNumber(resultI, resultJ, 0)), delay);
     return result;
   }
 
+  function decide(){
+    if (puzzle.complete === ValidationStates.EMPTY) {
+      if (unTried.length > 0) {
+        unTried = [...tried, ...unTried];
+        tried = [];
+        let trying = unTried.pop();
+        pushToGrid(trying);
+      } else {
+        tried.push(popFromGrid());
+      }
+    } else if (puzzle.complete === ValidationStates.INVALID) {
+      tried.push(popFromGrid());
+    }
+  }
 }
 
 export default AI;
